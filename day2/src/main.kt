fe9675463686a524b32e8570d4c998f3bf4b384c
filmchu <68175925+filmchu@users.noreply.github.com>
2020-08-02 11:31:28 +0700

    dog

--- day2/src/main.kt
+++ day2/src/main.kt
@@ -1,9 +1,11 @@
 fun main(){
-    var x = 1
-    while ( x < 3) {
-        print(if (x == 1)"Yab" else "Dab")
-        print("Ba")
-        x = x + 1
-    }
-    if (x == 3) println ("Do")
+
+    val d = DrumKit(true, true)
+    d.playTopHat()
+    d.playSnare()
+    d.hasSnare = false
+    d.playTopHat()
+    d.playSnare()
+
+
 }
