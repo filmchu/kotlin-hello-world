7b27e75138c93d26c1cd466fafedd3faefb2222b
filmchu <68175925+filmchu@users.noreply.github.com>
2020-08-16 10:54:51 +0700

    ลบไฟล์ที่ไม่ได้ใช้ทิ้ง

--- day2/src/DrumKit.kt
+++
@@ -1,8 +0,0 @@
-class DrumKit (var hasTopHat: Boolean, var hasTopHat: Boolean){
-    fun playTopHat(){
-        if (hasTopHat) println("ding ding ba-da-bing!")
-    }
-    fun playSnare(){
-        if (hasTopHat) println("bang bang bang!")
-    }
-}
