37a25cdcab069d7f4190657f73f46c941d4a44fa
filmchu <68175925+filmchu@users.noreply.github.com>
2020-08-30 13:06:17 +0700

    ทำเสร็จแล้ว

---
+++ day2/src/animal.kt
@@ -0,0 +1,14 @@
+abstract class Animal {
+    abstract val image: String
+    abstract val food: String
+    abstract val habitat: String
+    var hunger = 10
+    abstract fun makeNoise() {
+        println("The Animal is making a noise")
+        val animals = arrayOF(Hippo(),Wolf(),Lion(),Cheetah(),Lynx(),FoxX())
+        for (item in animal){
+            item.roam()
+            item
+        }
+    }
+}
